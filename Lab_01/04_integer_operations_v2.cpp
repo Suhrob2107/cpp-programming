ab3b38510ec70a77c0749462fe8cbceb9b3f8fc6
Sukhrobzhon Talibzhanov <tolibjanov05@gmail.com>
2025-06-08 03:45:42 +0500

    integer opertions v2

---
+++ Lab_01/04_integer_operations_v2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+    cout << "Sum: " << num1 + num2 << endl;
+    cout << "Difference: " << num1 - num2 << endl;
+    cout << "ProductL " << num1 * num2 << endl;
+    cout << "Quotient " << num1 / num2 << endl;
+    cout << "Remainder " << num1 % num2 << endl;
+    system("pause");
+    return 0;
+}
