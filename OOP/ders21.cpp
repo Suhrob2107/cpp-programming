71bd682ee6ce27487d574b075ff97cd15d5e7037
Sukhrobzhon Talibzhanov <tolibjanov05@gmail.com>
2026-01-17 12:42:04 +0500

    updated repository

--- OOP/ders21.cpp
+++ OOP/ders21.cpp
@@ -24,6 +24,12 @@ public:
     ~Kolon(){delete[] data;cout << "Yikici calisti. Dizi silindi" << endl;} //Destructor (Yıkıcı)
     const int* getData()const{return data;}
 
+    void set(int indis,int value)
+    {
+        if(indis < boyut)
+            data[indis] = value;
+    }
+
 
     int getBoyut()
     {
@@ -40,17 +46,63 @@ public:
         cout << endl;
     }
 
+    Kolon topla(const Kolon&k)const
+    {
+        if(boyut == k.boyut)
+        {
+            Kolon temp(boyut);
+            for(int i = 0;i < boyut;i++)
+            {
+                temp.data[i] = data[i] + k.data[i];
+            }
+            return temp;
+        }
+    }
+
+    Kolon carpma(const Kolon& k)const
+    {
+        if(boyut == k.boyut)
+        {
+            Kolon temp(boyut);
+            for(int i=0;i<boyut;i++)
+                temp.data[i] = data[i] * k.data[i];
+            return temp;
+        }
+    }
+
+    double ortHesapla()const
+    {
+        int ara = 0;
+        for(int i = 0;i < boyut;i++)
+            ara += data[i];
+        return (double)ara/boyut;
+    }
+
 
 };
 
 int main()
 {
-    Kolon k(5);
-    Kolon k2 = k;
+    Kolon k(3);
+    k.set(0,5);
+    k.set(1,3);
+    k.set(2,10);
     k.print();
+
+    Kolon k2(3);
+    k2.set(0,10);
+    k2.set(1,2);
+    k2.set(2,3);
     k2.print();
-    cout << k.getData() << endl;
-    cout << k2.getData() << endl;
+
+    cout << "Toplami: " << endl << endl;
+    (k.topla(k2)).print();
+    cout << "Carpimi: " << endl << endl;
+    (k.carpma(k2)).print();
+
+    cout << "K nin ortalamasi " << k.ortHesapla() << endl;
+    cout << "k2 nin ortalamasi " << k2.ortHesapla() << endl;
+    cout << "k + k2 Ortalamasi" << (k.carpma(k2)).ortHesapla();
 
     return 0;
 }
